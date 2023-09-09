6d89d3ddebc09d8c59b45821b208504ed5a08978
Hadisehzamani <hadisehzmn@gmail.com>
2023-09-10 01:00:36 +0330

    added addeventlistener to buttons for checking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 const $ = document
+let currentWord;
+let wrongGuessCounter = 0;
+let maxWrongGuess = 6;
 
 const keyboardContainer = $.querySelector('.keyboard-container')
 const wordDisplayContainer = $.querySelector('.word-display')
+const gussesText = $.querySelector('.gusses-text span')
 
 //getting random word
 const getRandomWord = () => {
@@ -10,17 +14,36 @@ const getRandomWord = () => {
     let chosenCategory = wordList[randomCategory];
     let randomIndex = Math.floor(Math.random() * chosenCategory.length);
     let randomWord = chosenCategory[randomIndex];
+    currentWord = randomWord.word.toLowerCase();
+    console.log(currentWord);
     $.querySelector('.hint span').innerText = randomWord.hint;
     $.querySelector('.subject span').innerText = randomCategory;
-    wordDisplayContainer.innerHTML = randomWord.word.split("").map(() => `<li class="letter"></li>`).join("")
+    wordDisplayContainer.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("")
 }
 
+//function for checking the letters that is existing ot not
+const clickedBtn = (btn , clickedLetter) => {
+    if(currentWord.includes(clickedLetter)){
+        //checking letter by letter
+        [...currentWord].forEach((letter, index) => {
+            if(letter === clickedLetter) {
+                wordDisplayContainer.querySelectorAll("li")[index].innerText = letter;
+                wordDisplayContainer.querySelectorAll("li")[index].classList.add('guessed');
+            }
+        })
+    }else {
+        wrongGuessCounter++;
+    }
+    gussesText.innerText = `${wrongGuessCounter} / ${maxWrongGuess}`;
+    btn.disabled = true;
+}
 
 //creating keyboard buttons
 for(let i = 97;i <= 122;i++) {
     const btn = $.createElement('button');
     btn.innerText = String.fromCharCode(i);
     keyboardContainer.appendChild(btn)
+    btn.addEventListener("click", e => clickedBtn(e.target, String.fromCharCode(i)))
 }
 
 getRandomWord()
