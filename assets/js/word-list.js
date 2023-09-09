0972e9a61b348329bd5aa78c7d078e88f9deb460
Hadisehzamani <hadisehzmn@gmail.com>
2023-09-09 23:12:05 +0330

    added list of words

---
+++ assets/js/word-list.js
@@ -0,0 +1,51 @@
+const wordList = {
+    programLanguage: [
+        {word: 'Javascript', hint: "The language of the web."},
+        {word: 'Python', hint: "Named after a British comedy group."},
+        {word: 'Java', hint: "Not just a cup of coffee."},
+        {word: 'Ruby', hint:  "Gems and Rails."},
+        {word: 'C++', hint: "An extension of a popular language."},
+        {word: 'Swift', hint:  "Apple's favorite."},
+        {word: 'PHP', hint: "Used to build dynamic web pages."},
+        {word: 'Rust', hint: "Promises memory safety."},
+        {word: 'Go', hint: "Simple and efficient."},
+        {word: 'Haskell', hint: "Functional programming elegance."}
+
+    ],
+    animal: [
+        { word: 'Elephant', hint: "The largest land animal." },
+        { word: 'Kangaroo', hint: "Known for its powerful hind legs." },
+        { word: 'Giraffe', hint: "Has the longest neck in the animal kingdom." },
+        { word: 'Penguin', hint: "Waddling flightless bird." },
+        { word: 'Cheetah', hint: "Fastest land animal." },
+        { word: 'Dolphin', hint: "Highly intelligent marine mammal." },
+        { word: 'Lion', hint: "King of the jungle." },
+        { word: 'Panda', hint: "Black and white bear from China." },
+        { word: 'Koala', hint: "Eucalyptus-loving marsupial." },
+        { word: 'Hippopotamus', hint: "Large, amphibious mammal." }
+      ],
+      fruit: [
+        { word: 'Banana', hint: "Yellow, curved, and potassium-rich." },
+        { word: 'Strawberry', hint: "Red, juicy, and heart-shaped." },
+        { word: 'Pineapple', hint: "Tropical, spiky fruit with a sweet core." },
+        { word: 'Watermelon', hint: "Green on the outside, red on the inside." },
+        { word: 'Grapes', hint: "Small, round, and often used to make wine." },
+        { word: 'Mango', hint: "Tropical fruit with a sweet and juicy flesh." },
+        { word: 'Kiwi', hint: "Small, fuzzy, and green on the inside." },
+        { word: 'Orange', hint: "Citrus fruit known for its vitamin C." },
+        { word: 'Apple', hint: "A fruit a day keeps the doctor away." },
+        { word: 'Cherry', hint: "Tiny, red or black, and often used in desserts." }
+      ],
+      sport: [
+        { word: 'Basketball', hint: "A sport played with a round ball and a hoop." },
+        { word: 'Soccer', hint: "Known as football in most parts of the world." },
+        { word: 'Tennis', hint: "A racquet sport played on a rectangular court." },
+        { word: 'Golf', hint: "A precision club-and-ball sport played on a course." },
+        { word: 'Swimming', hint: "Moving through water using strokes." },
+        { word: 'Baseball', hint: "A bat-and-ball game played on a diamond-shaped field." },
+        { word: 'Cricket', hint: "A bat-and-ball game popular in England and beyond." },
+        { word: 'Volleyball', hint: "A team sport played with a net." },
+        { word: 'Hockey', hint: "Played with a puck and sticks, on ice or grass." },
+        { word: 'Boxing', hint: "A combat sport with punches and knockouts." }
+      ]
+    };
